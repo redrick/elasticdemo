e2737c4a22d7827434c226ebfe8bac72dff98aba
Andrej Antas <andrej.antas@gmail.com>
2013-11-11 22:43:14 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Elasticdemo::Application.config.secret_key_base = '28e21a700c3a6e8215645820cb8d2af51a8e811467916d37a299d5e3a31c04c5e68953db21f4ca3a0b81b9fac4f0fd84a4dd2b15fa222473f3699d8a650fee6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Elasticdemo::Application.config.session_store :cookie_store, key: '_elasticdemo_session'
